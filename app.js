c4f6f8c3fb6cc089117b7a86b2e48f7fbd96b456
sehee <sehee@sh>
2016-12-14 22:46:00 +0900

    test

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var routes = require('./routes/example');
 var http = require('http');
 var path = require('path');
 
+
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
